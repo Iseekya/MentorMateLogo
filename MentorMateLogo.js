77fe59a81fbd57ec5881aef3c8e465a76b2a338f
Stefan Hitrov <stefanhitrov@Stefans-MacBook-Pro.local>
2020-02-17 21:56:56 +0200

    Refactoring code

--- MentorMateLogo.js
+++ MentorMateLogo.js
@@ -1,41 +1,53 @@
-function validInput(number) { // Check the input is correct!
-    if(typeof number !== 'number'){
-        console.log('Please enter numbers only!');
-    }else if (number > 2 && number < 10000 && number % 2 === 1) {
-        printMMLogo(number);
-    } else {
-        throw Error("Wrong Input!")
+var LogoPrinter = (function () {
+
+    const star = "*";
+    const dash = "-";
+
+
+    // Constructor
+    function LogoPrinter () {
     }
-}
 
-function printMMLogo(number){
+    // Check the input is correct!
+    function validateInput(number) {
+            if(typeof number !== 'number'){
+                console.log('Please enter numbers only!');
+            }else if (number > 2 && number < 10000 && number % 2 === 1) {
+                printMMLogo(number);
+            }else {
+                throw Error("Wrong Input!");
+            }
+        }
 
-let star = "*";
-let dash = "-";
 
-    firstNumToDecrease = number;
-    secondNumToDecrease = number;
-    thirdNumToIncrease = number;
+    function printMMLogo(number){
 
- 
-        for(let i = 0; i < number / 2; i++){ //prints the lines above the middile  of M
-
+            
+        let firstNumToDecrease = number;
+        let secondNumToDecrease = number;
+        let thirdNumToIncrease = number;
+            
+             
+        //prints the lines above the middile  of M
+        for(let i = 0; i < number / 2; i++){
+            
             line = dash.repeat(firstNumToDecrease) + 
             star.repeat(secondNumToDecrease) + 
             dash.repeat(thirdNumToIncrease) + 
             star.repeat(secondNumToDecrease) + 
             dash.repeat(firstNumToDecrease);
             console.log(line.repeat(2));
-
+            
             firstNumToDecrease--;
             secondNumToDecrease += 2;
             thirdNumToIncrease -= 2;
         }
-        for(let i = 0; i < number / 2; i++){ // // prints the lines  below the middle of M
-
+            // prints the lines  below the middle of M
+            for(let i = 0; i < number / 2; i++){
+            
             secondNumToDecrease -= 2;
             thirdNumToIncrease += 2;
-            
+                        
             line = dash.repeat(firstNumToDecrease) + 
             star.repeat(number) + 
             dash.repeat(thirdNumToIncrease) + 
@@ -46,9 +58,18 @@ let dash = "-";
             console.log(line.repeat(2));
             firstNumToDecrease--;
         }
+ }
 
+
+
+ LogoPrinter.prototype.publicFun = function (input) {
+        return validateInput.call(this, input);
     }
-    printMMLogo(9)
+        return LogoPrinter;
+ })();
+    
+    
+    let logoPrinter = new LogoPrinter();
 
-
+    logoPrinter.publicFun(9);
 
